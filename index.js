c9acbae0c212113f1d3887e3f8d3319bf6aea270
github username <github email address>
2018-10-08 07:56:01 -0400

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,42 @@
 // Code your solution here:
+// Yes sir
+
+function driversWithRevenueOver(drivers, revenue) {
+  highEarn = []
+  drivers.filter(function (driver) { if (driver.revenue > revenue) {
+    highEarn.push(driver)
+  } });
+  return highEarn
+}
+
+function driverNamesWithRevenueOver(drivers, revenue){
+  highEarn = []
+  drivers.filter(function (driver) { if (driver.revenue > revenue) {
+    highEarn.push(driver.name)
+  } });
+  return highEarn
+}
+
+function exactMatch(drivers, obj) {
+  match = []
+  drivers.filter(function(driver){
+    if (driver[Object.keys(obj)[0]] === obj[Object.keys(obj)[0]]) {
+      match.push(driver)
+    }
+  })
+  return match
+}
+
+function exactMatchToList(drivers, obj) {
+  match = []
+  drivers.filter(function(driver){
+    if (driver[Object.keys(obj)[0]] === obj[Object.keys(obj)[0]]) {
+      match.push(driver.name)
+    }
+  })
+  return match
+}
+
+// exactMatch(drivers, { revenue: 3000 })
+// exactMatch(drivers, { name: 'Bob' })
+// name: 'Sally',   revenue: 400
